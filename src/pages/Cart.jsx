08f629bda316de44053ffe331a26040c8d053961
lek <abungan082198@gmail.com>
2025-10-08 14:02:03 +0800

    updated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,34 +18,3 @@ export default function Cart() {
     </>
   );
 }
-
-// import { useSelector, useDispatch } from "react-redux";
-// import { removeFromCart, clearCart, myCart } from "../features/addCartSlice";
-
-// export default function Cart() {
-//   const { cartItems } = useSelector(myCart);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div>
-//       <h2>Cart</h2>
-//       {cartItems.length === 0 ? (
-//         <p>Your cart is empty.</p>
-//       ) : (
-//         <>
-//           <ul>
-//             {cartItems.map((item) => (
-//               <li key={item.id}>
-//                 {item.name} (x{item.quantity}) - ${item.price * item.quantity}
-//                 <button onClick={() => dispatch(removeFromCart(item.id))}>
-//                   Remove
-//                 </button>
-//               </li>
-//             ))}
-//           </ul>
-//           <button onClick={() => dispatch(clearCart())}>Clear Cart</button>
-//         </>
-//       )}
-//     </div>
-//   );
-// }
